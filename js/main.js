636459d46945765fb69e15fa2d6e48a3e5f7785c
deflea <c_stuffs@tutanota.com>
2021-10-28 21:59:01 -0300

    Correcion mensaje

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function validarNombre() {
       alert('El campo nombre no puede estar vacio');
       return false;
     } else if (nombre.value.length < 2){
-        alert('La longitud del campo no pude ser menor a 2 caracteres');
+        alert('La longitud del campo no puede ser menor a 2 caracteres');
         return false;
     }
     return true;
